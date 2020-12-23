ef37f76fe183ecb9a4b5158f3a54a2aaddec01b1
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2020-12-23 17:21:28 +1100

    count-up implemented

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {Card, CardContent, Typography, Grid} from "@material-ui/core"
+import styles from "./Cards.module.css";
+import CountUp from "react-countup"
+
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+    if(!confirmed){
+        return 'Loading...'
+    }
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Infected
+                        </Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">
+                            REAL DATE
+                        </Typography>
+                        <Typography variant="body2">
+                            Number of Active Cases
+                        </Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Recovered
+                        </Typography>
+                        <Typography variant="h5">
+                            REAL DATA
+                        </Typography>
+                        <Typography color="textSecondary">
+                            REAL DATE
+                        </Typography>
+                        <Typography variant="body2">
+                            Number of Recoveries
+                        </Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Deaths
+                        </Typography>
+                        <Typography variant="h5">
+                            REAL DATA
+                        </Typography>
+                        <Typography color="textSecondary">
+                            REAL DATE
+                        </Typography>
+                        <Typography variant="body2">
+                            Number of Deaths
+                        </Typography>
+                    </CardContent>
+                </Grid>
+
+            </Grid>
+
+
+        </div>
+
+    )
+
+}
+
+export default Cards;
